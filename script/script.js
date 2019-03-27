7726b251eb4cc29029e4087a6d7aeb70047c7dc0
Jonathan Dyason <jonathandyason@hotmail.com>
2019-03-27 21:08:03 +0200

    f

---
+++ script/script.js
@@ -0,0 +1,106 @@
+
+
+
+
+// checking answers in "My Family" section
+
+$("#submit1").click(function (e) {
+    if ($("#question").val() == 1) {
+        $('#result').text("correct").addClass('bg-success').removeClass('bg-danger');
+    }
+    else {
+        $('#result').text("incorrect").addClass('bg-danger').removeClass('bg-success');
+    }
+});
+
+$("#submit2").click(function (e) {
+    if ($("#question").val() == 3) {
+        $('#result').text("correct").addClass('bg-success').removeClass('bg-danger');
+    }
+    else {
+        $('#result').text("incorrect").addClass('bg-danger').removeClass('bg-success');
+    }
+});
+
+$("#submit3").click(function (e) {
+    if ($("#question").val() == 4) {
+        $('#result').text("correct").addClass('bg-success').removeClass('bg-danger');
+    }
+    else {
+        $('#result').text("incorrect").addClass('bg-danger').removeClass('bg-success');
+    }
+});
+
+$("#submit4").click(function (e) {
+    if ($("#question").val() == 3) {
+        $('#result').text("correct").addClass('bg-success').removeClass('bg-danger');
+    }
+    else {
+        $('#result').text("incorrect").addClass('bg-danger').removeClass('bg-success');
+    }
+});
+
+$("#submit5").click(function (e) {
+    if ($("#question").val() == 1) {
+        $('#result').text("correct").addClass('bg-success').removeClass('bg-danger');
+    }
+    else {
+        $('#result').text("incorrect").addClass('bg-danger').removeClass('bg-success');
+    }
+});
+
+
+
+// text transition on website page
+
+
+// $('#website-intro-line1').animate({'opacity': 0}, 3000, function () {
+//     $(this).text('学英语');
+// }).animate({'opacity': 1}, 3000);
+
+
+
+// $('#website-intro-line2').animate({'opacity': 0}, 3000, function () {
+//     $(this).text('与漫画');
+// }).animate({'opacity': 1}, 3000);
+
+
+
+
+var textArray1 = ["学英语","выучить английский язык","تعلم الإنجليز", "Learn English"];
+var textArray2 = ["与漫画","с комиксами","مع كاريكاتير", "with comics"];
+var index1 = 0;
+var index2 = 0;
+
+setInterval(function(){        
+$("#changeText1").animate({
+opacity:0
+},function()
+{
+   if(textArray1.length > index1) {
+   $(this).text(textArray1[index1]).animate({opacity:1})
+   index1++; 
+   }
+   else
+   index1 = 0;
+});
+},2000);
+
+
+setInterval(function(){   
+$("#changeText2").animate({
+    opacity:0
+    },function()
+    {
+       if(textArray2.length > index2) {
+       $(this).text(textArray2[index2]).animate({opacity:1})
+       index2++; 
+       }
+       else
+       index2 = 0;
+    });
+},2000);
+
+
+
+
---
+++ views/artemis-and-the-animals/house-animals.php
@@ -0,0 +1,26 @@
+<?php
+    include "../partials/header.php";
+?>
+
+
+    <div class="main container">
+        <div class="row justify-content-center mt-3">
+            <img src="../../images/artemis-and-the-animals/house-animals.svg" class="img-fluid">
+        </div>
+
+        <div class="row mt-3">
+            <div class="col">
+                <img src="../../images/artemis-and-the-animals/artemis-005.jpg" class="img-fluid mx-auto d-block">
+            </div>
+        </div>
+
+        <div class="row mt-2">
+            <a class="btn btn-outline-dark mx-auto" href="farm-animals.php" role="button">next</a>
+        </div>
+    </div>
+
+    
+<?php
+    include "../partials/footer.php";
+?>
+
